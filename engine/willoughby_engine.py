b780aa6207274f1616b84879409b785e490c3340
Junhan <stoneloe.m@gmail.com>
2023-06-08 15:24:56 -0700

    refactor engine component

--- engine/willoughby_engine.py
+++ engine/willoughby_engine.py
@@ -1,13 +1,13 @@
-from abc import ABC
-
-from car import Car
+from engine.engine import Engine
 
 
-class WilloughbyEngine(Car, ABC):
-    def __init__(self, last_service_date, current_mileage, last_service_mileage):
-        super().__init__(last_service_date)
+class WilloughbyEngine(Engine):
+    def __init__(self, current_mileage, last_service_mileage):
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
+        self.criteria = 60_000
 
-    def engine_should_be_serviced(self):
-        return self.current_mileage - self.last_service_mileage > 60000
+    def needs_service(self):
+        return self.current_mileage - self.last_service_mileage >= 60_000
+
+
