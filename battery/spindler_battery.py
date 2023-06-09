98cd0a7a2422cbedce0295da00543af6e160de57
Junhan <stoneloe.m@gmail.com>
2023-06-08 18:53:21 -0700

    Update new battery criteria

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -6,7 +6,7 @@ class SpindlerBattery(Battery):
     def __init__(self, last_service_date, current_date):
         self.last_service_date = last_service_date
         self.current_date = current_date
-        self.criteria = 2
+        self.criteria = 3
 
     def needs_service(self):
         return service_date(self.last_service_date, self.criteria) <= self.current_date
